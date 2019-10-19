d070a7511b6ce3d44345d9b25c1f5acc83922cce
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-10-19 22:03:10 +0100

    Add jquery accordion

---
+++ function-jquery.js
@@ -0,0 +1,8 @@
+$('div.s_toggle').click(function(){
+  if ( $(this).next().is(":visible")){
+    $(this).next().hide('slow');
+  } else{
+    $('div.nascosto:visible').hide('slow');
+    $(this).next().show('slow');
+  }
+});
